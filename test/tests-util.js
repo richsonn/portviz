6c1caa1a179e29e62781be97d395cc0aa47a3cc1
joel <joel@truher.org>
2013-02-01 13:20:16 -0800

    oops, fix test, thanks jenkins

--- test/tests-util.js
+++ test/tests-util.js
@@ -1,50 +1,50 @@
-/*global money:false, boxmuller:false */
+/*global portviz:false*/
 test("money.fmt args", function() {
-    throws( function() { money.fmt('a') ;});
+    throws( function() { portviz.money.fmt('a') ;});
 });
 test("money.custom args", function() {
-    throws( function() { money.custom('a') ;});
+    throws( function() { portviz.money.custom('a') ;});
 });
 test("money.custom args", function() {
-    throws( function() { money.custom('a','b','c','d') ;});
+    throws( function() { portviz.money.custom('a','b','c','d') ;});
 });
 test("money.fmt", function() {
-    var f = money.fmt();
+    var f = portviz.money.fmt();
     var actual = f(10);
     var expected = "10";
     equal(actual, expected);
 });
 
 test("money.custom", function() {
-    var f = money.custom(2, '.', ',');
+    var f = portviz.money.custom(2, '.', ',');
     var actual = f(10);
     var expected = "10.00";
     equal(actual, expected);
 });
 
 test("large money.fmt", function() {
-    var f = money.fmt();
+    var f = portviz.money.fmt();
     var actual = f(123456.789);
     var expected = "123,457";
     equal(actual, expected);
 });
 
 test("large money.custom", function() {
-    var f = money.custom(2, '.', ',');
+    var f = portviz.money.custom(2, '.', ',');
     var actual = f(123456.789);
     var expected = "123,456.79";
     equal(actual, expected);
 });
 
 test("rand", function() {
-    var r = boxmuller.newInstance();
+    var r = portviz.boxmuller.newInstance();
     var rr = r.random();
     ok(!isNaN(rr));
 });
 
 test("rand seeded", function() {
-    var r1 = boxmuller.newInstance(1);
-    var r2 = boxmuller.newInstance(1);
+    var r1 = portviz.boxmuller.newInstance(1);
+    var r2 = portviz.boxmuller.newInstance(1);
     var rr11 = r1.random();
     var rr21 = r2.random();
     equal(rr11, rr21);
