cd938e674cfdabd9dd576e1ba603a1a2b00fac25
joel <joel@truher.org>
2013-02-05 19:21:41 -0800

    clean up knapsack for blog

--- src/optimization/knapsack.js
+++ src/optimization/knapsack.js
@@ -1,14 +1,19 @@
 /*global portviz:false, _:false */
 /*
- * after rosettacode.org/mw/index.php?title=Knapsack_problem/0-1
+ * 0-1 knapsack solution, recursive, memoized, approximate.
  *
- * approximation: http://math.mit.edu/~goemans/18434S06/knapsack-katherine.pdf
+ * credits:
+ *
+ * the Go implementation here:
+ *   http://rosettacode.org/mw/index.php?title=Knapsack_problem/0-1
+ *
+ * approximation details here:
+ *   http://math.mit.edu/~goemans/18434S06/knapsack-katherine.pdf
  */
 portviz.knapsack = {};
 (function() {
   this.combiner = function(items, weightfn, valuefn) {
-    // total value >= (1-e) * the optimal value
-    // i.e. e=0.1, get over 90% of the optimal
+    // approximation guarantees result >= (1-e) * optimal
     var _epsilon = 0.01;
     var _p = _.max(_.map(items,valuefn));
     var _k = _epsilon * _p / items.length;
@@ -23,44 +28,40 @@ portviz.knapsack = {};
           return _mem[_key(i,w)];
         },
         put: function(i, w, r) {
-          //console.log('put ' + i + ' ' + w);
-          //console.log(r);
           _mem[_key(i,w)]=r;
           return r;
         }
       };
     })();
+
     var _m = function(i, w) {
-      //console.log('item ' + i + ' weight ' + w);
 
       i = Math.round(i);
       w = Math.round(w);
 
 
       if (i < 0 || w === 0) {
-        //console.log('empty, base case');
-        //return _memo.put(i, w, {items: [], totalWeight: 0, totalValue: 0});
+        // empty base case
         return {items: [], totalWeight: 0, totalValue: 0};
       }
 
       var mm = _memo.get(i,w);
       if (!_.isUndefined(mm)) {
-        //console.log('from memo');
         return mm;
       }
 
       var item = items[i];
       if (weightfn(item) > w) {
-        //console.log('item does not fit, try the next item');
+        //item does not fit, try the next item
         return _memo.put(i, w, _m(i-1, w));
       }
       // this item could fit.
-      //console.log('are we better off excluding it?')
+      // are we better off excluding it?
       var excluded = _m(i-1, w);
-      //console.log('or including it?')
+      // or including it?
       var included = _m(i-1, w - weightfn(item));
       if (included.totalValue + Math.floor(valuefn(item)/_k) > excluded.totalValue) {
-        //console.log('better off including it');
+        // better off including it
         // make a copy of the list
         var i1 = included.items.slice();
         i1.push(item);
@@ -69,7 +70,7 @@ portviz.knapsack = {};
            totalWeight: included.totalWeight + weightfn(item),
            totalValue: included.totalValue + Math.floor(valuefn(item)/_k)});
       }
-      //console.log('better off excluding it.');
+      //better off excluding it
       return _memo.put(i,w, excluded);
     };
     return {
@@ -85,14 +86,12 @@ portviz.knapsack = {};
       /* the entire EF */
       ef: function(maxweight, step) {
         return _.map(_.range(0, maxweight+1, step), function(weight) {
-          //console.log("weight: " + weight);
           var scaled = _m(items.length - 1, weight);
           return {
             items: scaled.items,
             totalWeight: scaled.totalWeight,
             totalValue: scaled.totalValue * _k
           };
-          //return _m(items.length - 1, weight);
         });
       }
     };
